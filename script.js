0e3049cca8f55d912d731d0bed1193dd0f89019a
Jericho Betton <jerichobetton@gmail.com>
2023-10-09 21:43:45 -0400

    deleted test and added script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//FUNCTION ONE
+function hellowWorld() {
+    console.log("Hello, World!")
+  };
+
+  hellowWorld();
+
+  //FUNCTION TEW
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(5, 50)
+
+//Function THREE
+function greet(name){
+    
+    return("Hola, " + name + "! Como estas?")
+}
+let returnedValue = greet("Dylan");
+
+console.log(returnedValue);
