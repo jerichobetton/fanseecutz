d6e2e450330beda4370012ea3d272689be9b08d4
Jericho Betton <jerichobetton@gmail.com>
2023-10-20 21:39:26 -0400

    improvements

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-//FUNCTION ONE
-function hellowWorld() {
-    console.log("Hello, World!")
-  };
+let game = "Game: mario";
+let character = "Character: luigi";
+console.log(game);
+console.log(character);
+game = "Game: zelda";
+character = "Character: midna";
+console.log(game);
+console.log(character);
+character = "Character Switch: link";
+console.log(character);
 
-  hellowWorld();
+//FUNCTIONS
 
-  //FUNCTION TEW
-function add(a, b) {
-    console.log(a + b);
-}
+function sayMyName(name) {
+    console.log("Salut! Tu t'appelles " + name + "!")
+};
 
-add(5, 50)
+sayMyName("King Josh");
 
-//Function THREE
-function greet(name){
-    
-    return("Hola, " + name + "! Como estas?")
-}
-let returnedValue = greet("Dylan");
+function cityAndState(city, state) {
+    console.log("You live in " + city + ", " + state)
+};
 
-console.log(returnedValue);
+cityAndState("Columbus", "Ohio");
+
+function whoAmI(name, city, state) {
+    console.log("I think I know you! Your name is " + name + ", and you're from " + city + ", " + state + "!")
+};
+
+whoAmI("EhLieAsssss", "Windhelm", "Skyrim");
+//let myName = "King Josh";
+function todaysForecast(temp, humidity, windDir, windSpeed, outsideCondition) {
+    console.log("Oh, " + name + "! I forgot to let you know that it'll be " + temp + " degrees kelvin outside. " + "The humidity is " + humidity 
+    + ", so depending on your lungs, you may or may not need a mask. The wind is soaring to the " + windDir + " at a speed of " 
+    + windSpeed + ", so if you plan to glide, keep that in mind... Oh! There's also a slight chance of " + outsideCondition + " this afternoon. Godspeed!")
+};
+
+todaysForecast(92, 306, "West", "525,600 MPH", "dragons");
+
